d0bdfd29b96a00b8756f43c6820d64564f3fae6a
Rian Pollock <rian.pollock@gmail.com>
2025-03-12 07:35:36 +0000

    src/app update

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,1 +1,7 @@
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+import { createThirdwebClient } from 'thirdweb';
+
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+
+export const client = createThirdwebClient({
+    clientId: CLIENT_ID,
+});
